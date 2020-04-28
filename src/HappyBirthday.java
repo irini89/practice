9ed5ec3a627e9ca6d30c4db5459704cdda598fd6
Desara Irini <64229373+irini89@users.noreply.github.com>
2020-04-28 15:52:09 -0400

    Update HappyBirthday.java

--- src/HappyBirthday.java
+++ src/HappyBirthday.java
@@ -2,5 +2,10 @@ public class HappyBirthday {
 
     public static void main(String[] args) {
         System.out.println("Birthday");
+        
+        
+        System.out.println("Birthday girl");
+        
+        System.out.println("Birthday girl is happy");
     }
 }
